19516770f615d1e8a7f4996184e4c616d2000edb
Sven Nierlein <sven@consol.de>
2023-03-23 17:17:57 +0100

    try to build msi

---
+++ snclient_windows.go
@@ -0,0 +1,32 @@
+package snclient
+
+import (
+	"os"
+	"syscall"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func setupUsrSignalChannel(osSignalUsrChannel chan os.Signal) {
+}
+
+func mainSignalHandler(sig os.Signal, snc *Agent) MainStateType {
+	switch sig {
+	case syscall.SIGTERM:
+		log.Infof("got sigterm, quiting gracefully")
+
+		return ShutdownGraceFully
+	case os.Interrupt, syscall.SIGINT:
+		log.Infof("got sigint, quitting")
+
+		return Shutdown
+	case syscall.SIGHUP:
+		log.Infof("got sighup, reloading configuration...")
+
+		return Reload
+	default:
+		log.Warnf("Signal not handled: %v", sig)
+	}
+
+	return Resume
+}
