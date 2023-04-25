b6348ead56cf22652d0d4588df5c83b26ff995ed
a-pichler <andreas.pichler@consol.de>
2023-04-25 13:46:15 +0200

    added check_network and check_process_windows

---
+++ check_network.go
@@ -0,0 +1,95 @@
+package snclient
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/shirou/gopsutil/v3/net"
+	"golang.org/x/exp/slices"
+)
+
+func init() {
+	AvailableChecks["check_network"] = CheckEntry{"check_network", new(CheckNetwork)}
+}
+
+type CheckNetwork struct {
+	noCopy noCopy
+	data   CheckData
+}
+
+/* check_network
+ * Description: Checks the state of network interfaces
+ */
+func (l *CheckNetwork) Check(args []string) (*CheckResult, error) {
+	// default state: OK
+	state := int64(CheckExitOK)
+	var output string
+	l.data.detailSyntax = "%(name) >%(sent) <%(received) bps"
+	l.data.topSyntax = "%(list)"
+	l.data.okSyntax = "%(list)"
+	ParseArgs(args, &l.data)
+	var checkData map[string]string
+
+	interfaceList, _ := net.Interfaces()
+	IOList, _ := net.IOCounters(true)
+
+	metrics := make([]*CheckMetric, 0, len(interfaceList))
+	okList := make([]string, 0, len(interfaceList))
+	warnList := make([]string, 0, len(interfaceList))
+	critList := make([]string, 0, len(interfaceList))
+
+	for intnr, int := range interfaceList {
+		mdata := map[string]string{
+			"MAC":               int.HardwareAddr,
+			"enabled":           strconv.FormatBool(slices.Contains(int.Flags, "up")),
+			"name":              int.Name,
+			"net_connection_id": int.Name,
+			"received":          strconv.FormatUint(IOList[intnr].BytesRecv, 10),
+			"sent":              strconv.FormatUint(IOList[intnr].BytesSent, 10),
+			"speed":             "-1",
+		}
+
+		if CompareMetrics(mdata, l.data.critTreshold) && l.data.critTreshold.value != "none" {
+			critList = append(critList, ParseSyntax(l.data.detailSyntax, mdata))
+
+			continue
+		}
+
+		if CompareMetrics(mdata, l.data.warnTreshold) && l.data.warnTreshold.value != "none" {
+			warnList = append(warnList, ParseSyntax(l.data.detailSyntax, mdata))
+
+			continue
+		}
+
+		okList = append(okList, ParseSyntax(l.data.detailSyntax, mdata))
+	}
+
+	totalList := append(okList, append(warnList, critList...)...)
+
+	if len(critList) > 0 {
+		state = CheckExitCritical
+	} else if len(warnList) > 0 {
+		state = CheckExitWarning
+	}
+
+	checkData = map[string]string{
+		"count":        strconv.FormatInt(int64(len(totalList)), 10),
+		"ok_list":      strings.Join(okList, ", "),
+		"warn_list":    strings.Join(warnList, ", "),
+		"crit_list":    strings.Join(critList, ", "),
+		"list":         strings.Join(totalList, ", "),
+		"problem_list": strings.Join(append(critList, warnList...), ", "),
+	}
+
+	if state == CheckExitOK {
+		output = ParseSyntax(l.data.okSyntax, checkData)
+	} else {
+		output = ParseSyntax(l.data.topSyntax, checkData)
+	}
+
+	return &CheckResult{
+		State:   state,
+		Output:  output,
+		Metrics: metrics,
+	}, nil
+}
---
+++ check_process_windows.go
@@ -0,0 +1,177 @@
+package snclient
+
+import (
+	"internal/wmi"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"golang.org/x/exp/slices"
+)
+
+func init() {
+	AvailableChecks["check_process"] = CheckEntry{"check_process", new(CheckProcess)}
+}
+
+type CheckProcess struct {
+	noCopy noCopy
+	data   CheckData
+}
+
+var ProcessStates = map[string]string{
+	"stopped": "0",
+	"started": "1",
+	"0":       "stopped",
+	"1":       "started",
+}
+
+/* check_process_windows
+ * Description: Checks the state of a process on the host.
+ * Tresholds: status
+ * Units: ?
+ */
+func (l *CheckProcess) Check(args []string) (*CheckResult, error) {
+	// default state: OK
+	state := int64(CheckExitOK)
+	var output string
+	l.data.detailSyntax = "%(process)=%(state)"
+	l.data.topSyntax = "%(problem_list)"
+	l.data.okSyntax = "all processes are ok."
+	l.data.emptySyntax = "No processes found"
+	l.data.emptyState = CheckExitUnknown
+	argList := ParseArgs(args, &l.data)
+	var processes []string
+	var checkData map[string]string
+
+	// parse treshold args
+	for _, arg := range argList {
+		if arg.key == "process" {
+			processes = append(processes, arg.value)
+		}
+	}
+
+	metrics := make([]*CheckMetric, 0, len(processes))
+	okList := make([]string, 0, len(processes))
+	warnList := make([]string, 0, len(processes))
+	critList := make([]string, 0, len(processes))
+	processData, _ := wmi.Query(`Select
+									Name, 
+									CommandLine,
+									CreationDate,
+									ExecutablePath,
+									HandleCount,
+									KernelModeTime,
+									PageFileUsage,
+									PeakPageFileUsage,
+									PeakVirtualSize,
+									PeakWorkingSetSize,
+									ProcessId,
+									WorkingSetSize,
+									VirtualSize,
+									UserModeTime,
+									ThreadCount
+								From 
+									Win32_Process
+								`)
+	runningProcs := map[string]map[string]string{}
+
+	// collect process state
+	for _, process := range processData {
+		var name string
+		for _, proc := range process {
+			if proc.Key == "Name" {
+				name = proc.Value
+				runningProcs[name] = map[string]string{}
+				if slices.Contains(processes, "*") {
+					processes = append(processes, name)
+				}
+			}
+
+			runningProcs[name][proc.Key] = proc.Value
+		}
+	}
+
+	for _, process := range processes {
+		proc, exists := runningProcs[process]
+		if !exists {
+			continue
+		}
+
+		var state float64
+
+		if proc["ProcessId"] == "0" || proc["ThreadCount"] == "0" {
+			state = 0
+		} else {
+			state = 1
+		}
+
+		metrics = append(metrics, &CheckMetric{Name: process, Value: state})
+		dre := regexp.MustCompile(`\d+\.\d+`)
+		mdata := map[string]string{
+			"process":          process,
+			"state":            ProcessStates[strconv.FormatFloat(state, 'f', 0, 64)],
+			"command_line":     proc["CommandLine"],
+			"creation":         dre.FindString(proc["CreationDate"]),
+			"exe":              process,
+			"filename":         proc["ExecutablePath"],
+			"handles":          proc["HandleCount"],
+			"kernel":           proc["KernelModeTime"],
+			"pagefile":         proc["PageFileUsage"],
+			"peak_pagefile":    proc["PeakPageFileUsage"],
+			"peak_virtual":     proc["PeakVirtualSize"],
+			"peak_working_set": proc["PeakWorkingSetSize"],
+			"pid":              proc["ProcessId"],
+			"user":             proc["UserModeTime"],
+			"virtual":          proc["VirtualSize"],
+			"working_set":      proc["WorkingSetSize"],
+		}
+
+		if CompareMetrics(mdata, l.data.critTreshold) && l.data.critTreshold.value != "none" {
+			critList = append(critList, ParseSyntax(l.data.detailSyntax, mdata))
+
+			continue
+		}
+
+		if CompareMetrics(mdata, l.data.warnTreshold) && l.data.warnTreshold.value != "none" {
+			warnList = append(warnList, ParseSyntax(l.data.detailSyntax, mdata))
+
+			continue
+		}
+
+		okList = append(okList, ParseSyntax(l.data.detailSyntax, mdata))
+	}
+
+	totalList := append(okList, append(warnList, critList...)...)
+
+	if len(critList) > 0 {
+		state = CheckExitCritical
+	} else if len(warnList) > 0 {
+		state = CheckExitWarning
+	}
+
+	if len(totalList) == 0 {
+		state = l.data.emptyState
+	}
+
+	checkData = map[string]string{
+		"state":        ProcessStates[strconv.FormatInt(state, 10)],
+		"count":        strconv.FormatInt(int64(len(totalList)), 10),
+		"ok_list":      strings.Join(okList, ", "),
+		"warn_list":    strings.Join(warnList, ", "),
+		"crit_list":    strings.Join(critList, ", "),
+		"list":         strings.Join(totalList, ", "),
+		"problem_list": strings.Join(append(critList, warnList...), ", "),
+	}
+
+	if state == CheckExitOK {
+		output = ParseSyntax(l.data.okSyntax, checkData)
+	} else {
+		output = ParseSyntax(l.data.topSyntax, checkData)
+	}
+
+	return &CheckResult{
+		State:   state,
+		Output:  output,
+		Metrics: metrics,
+	}, nil
+}
