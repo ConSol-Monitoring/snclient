06d926196d90c5bd49756d51c53d4a9003f7119d
Sven Nierlein <sven@consol.de>
2023-05-05 15:31:05 +0200

    add windows specific check test

---
+++ checks_windows_test.go
@@ -0,0 +1,19 @@
+package snclient
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCheckEventlog(t *testing.T) {
+	snc := Agent{}
+	res := snc.RunCheck("check_eventlog", []string{})
+	assert.Equalf(t, CheckExitOK, res.State, "state OK")
+	assert.Regexpf(t,
+		regexp.MustCompile(`^Event log seems fine`),
+		string(res.BuildPluginOutput()),
+		"output matches",
+	)
+}
