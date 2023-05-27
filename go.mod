775721b69026970dceffb31aba4e9bee6bb7dca4
Sven Nierlein <sven@nierlein.de>
2023-05-28 00:35:04 +0200

    add snclient test mode

--- go.mod
+++ go.mod
@@ -164,6 +164,8 @@ require (
 	github.com/quasilyte/gogrep v0.5.0 // indirect
 	github.com/quasilyte/regex/syntax v0.0.0-20210819130434-b3f0c404a727 // indirect
 	github.com/quasilyte/stdinfo v0.0.0-20220114132959-f7386bf02567 // indirect
+	github.com/reeflective/readline v0.1.0-beta.0.20230523012110-bad9da7f3303 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/ryancurrah/gomodguard v1.3.0 // indirect
 	github.com/ryanrolds/sqlclosecheck v0.4.0 // indirect
 	github.com/sanposhiho/wastedassign/v2 v2.0.7 // indirect
@@ -221,6 +223,7 @@ require (
 	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/sync v0.2.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/term v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
---
+++ pkg/snclient/cmd/test.go
@@ -0,0 +1,100 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"pkg/snclient"
+	"pkg/utils"
+
+	"github.com/reeflective/readline"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	testCmd := &cobra.Command{
+		Use:   "test [cmd]",
+		Short: "Start test mode or run given query",
+		Long: `Test mode can be used to manually test queries.
+
+If query is given a one shot result will be printed. Without command, snclient
+will start a query prompt.
+
+Examples:
+
+# start query prompt:
+snclient test
+
+# run check_load and exit
+snclient test check_load filter=none crit=none warn=none
+
+# run check_memory with debug output
+snclient test -vv check_memory
+`,
+		Run: func(cmd *cobra.Command, args []string) {
+			agentFlags.LogFile = "stdout"
+			agentFlags.LogFormat = snclient.LogColors + `[%{Time "15:04:05.000"}][%{S}] %{Message}` + snclient.LogColorReset
+			if agentFlags.Verbose > 2 {
+				agentFlags.LogFormat = snclient.LogColors + `[%{Time "15:04:05.000"}][%{S}][%{ShortFile}:%{Line}] %{Message}` + snclient.LogColorReset
+			}
+			snc := snclient.NewAgent(agentFlags)
+			if len(args) == 0 {
+				testPrompt(cmd, snc)
+
+				return
+			}
+			rc := testRunCheck(cmd, snc, args)
+			os.Exit(int(rc))
+		},
+	}
+	rootCmd.AddCommand(testCmd)
+}
+
+func testPrompt(cmd *cobra.Command, snc *snclient.Agent) {
+	promptCompleter := func(line []rune, cursor int) readline.Completions {
+		checks := []string{}
+		filter := string(line[0:cursor])
+		for chk := range snclient.AvailableChecks {
+			if strings.HasPrefix(chk, filter) {
+				checks = append(checks, chk)
+			}
+		}
+		return readline.CompleteValues(checks...)
+	}
+
+	snc.PrintVersion()
+
+	rl := readline.NewShell()
+	rl.Prompt.Primary(func() string { return ">> " })
+	rl.Config.Set("show-mode-in-prompt", false)
+	rl.Completer = promptCompleter
+	for {
+		text, err := rl.Readline()
+		if err != nil {
+			break
+		}
+		switch text {
+		case "", "exit":
+			break
+		default:
+			args := utils.Tokenize(text)
+			testRunCheck(cmd, snc, args)
+		}
+	}
+}
+
+func testRunCheck(cmd *cobra.Command, snc *snclient.Agent, args []string) int64 {
+	res := snc.RunCheck(args[0], args[1:])
+	fmt.Fprintf(rootCmd.OutOrStdout(), "Exit Code: %s (%d)\n", res.StateString(), res.State)
+	fmt.Fprintf(rootCmd.OutOrStdout(), "Plugin Output:\n")
+	fmt.Fprintf(rootCmd.OutOrStdout(), "%s\n", res.Output)
+	if len(res.Metrics) > 0 {
+		fmt.Fprintf(rootCmd.OutOrStdout(), "\nPerformance Metrics:\n")
+		for _, m := range res.Metrics {
+			fmt.Fprintf(rootCmd.OutOrStdout(), "  - %s\n", m.String())
+		}
+	}
+
+	return res.State
+}
