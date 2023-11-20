5e351bb44a59eb3efd8e0f4926b61e19a434a63e
Sven Nierlein <sven@consol.de>
2023-11-20 23:31:40 +0100

    docs: fix test for missing docs

--- pkg/snclient/docs_test.go
+++ pkg/snclient/docs_test.go
@@ -15,18 +15,24 @@ func TestDocsExists(t *testing.T) {
 		if name == "check_index" {
 			continue
 		}
-		pluginFile := fmt.Sprintf("docs/checks/plugins/%s.md", name)
-		commandFile := fmt.Sprintf("docs/checks/commands/%s.md", name)
+		pluginFile := fmt.Sprintf("../../docs/checks/plugins/%s.md", name)
+		commandFile := fmt.Sprintf("../../docs/checks/commands/%s.md", name)
 		isPlugin := false
 		isCommand := false
-		if _, err := os.Stat(pluginFile); os.IsNotExist(err) {
+		if _, err := os.Stat(pluginFile); err == nil {
 			isPlugin = true
 		}
-		if _, err := os.Stat(commandFile); os.IsNotExist(err) {
+		if _, err := os.Stat(commandFile); err == nil {
 			isCommand = true
 		}
+		for _, osName := range []string{"windows", "linux"} {
+			commandFile := fmt.Sprintf("../../docs/checks/commands/%s_%s.md", name, osName)
+			if _, err := os.Stat(commandFile); err == nil {
+				isCommand = true
+			}
+		}
 		if !isCommand && !isPlugin {
-			assert.Failf(t, "docs exist", "docs/checks/... file for %s does not exist", name)
+			assert.Failf(t, "docs exist", "docs/checks/.../%s.md file for %s does not exist", name, name)
 		}
 	}
 
