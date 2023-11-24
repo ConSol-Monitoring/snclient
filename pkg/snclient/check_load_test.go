2456a24aec8c14ee19ba551b11018f266f4b4df4
Sven Nierlein <sven@consol.de>
2023-11-24 19:46:16 +0100

    test: fix load test having more than 10 cores

--- pkg/snclient/check_load_test.go
+++ pkg/snclient/check_load_test.go
@@ -44,7 +44,7 @@ func TestCheckLoad(t *testing.T) {
 	res = snc.RunCheck("check_load", []string{"warn=load > 0", "crit=load > 0"})
 	assert.Equalf(t, CheckExitCritical, res.State, "state Critical")
 	assert.Regexpf(t,
-		regexp.MustCompile(`^CRITICAL: total load average: [\d\.]+, [\d\.]+, [\d\.]+ at \d cores \|'load1'=[\d\.]+;0;0;0 'load5'=[\d\.]+;0;0;0 'load15'=[\d\.]+;0;0;0$`),
+		regexp.MustCompile(`^CRITICAL: total load average: [\d\.]+, [\d\.]+, [\d\.]+ at \d+ cores \|'load1'=[\d\.]+;0;0;0 'load5'=[\d\.]+;0;0;0 'load15'=[\d\.]+;0;0;0$`),
 		string(res.BuildPluginOutput()),
 		"output matches",
 	)
@@ -52,7 +52,7 @@ func TestCheckLoad(t *testing.T) {
 	res = snc.RunCheck("check_load", []string{"-w", "0,0,0", "-c", "999,998,997"})
 	assert.Equalf(t, CheckExitWarning, res.State, "state Warning")
 	assert.Regexpf(t,
-		regexp.MustCompile(`^WARNING: total load average: [\d\.]+, [\d\.]+, [\d\.]+ at \d cores \|'load1'=[\d\.]+;0;999;0 'load5'=[\d\.]+;0;998;0 'load15'=[\d\.]+;0;997;0$`),
+		regexp.MustCompile(`^WARNING: total load average: [\d\.]+, [\d\.]+, [\d\.]+ at \d+ cores \|'load1'=[\d\.]+;0;999;0 'load5'=[\d\.]+;0;998;0 'load15'=[\d\.]+;0;997;0$`),
 		string(res.BuildPluginOutput()),
 		"output matches",
 	)
