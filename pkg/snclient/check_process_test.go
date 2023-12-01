fb61d3b869ca2f9da6f5489a248a7dabd992a67e
Sven Nierlein <sven@consol.de>
2023-12-01 13:33:34 +0100

    add test case

--- pkg/snclient/check_process_test.go
+++ pkg/snclient/check_process_test.go
@@ -13,12 +13,16 @@ func TestCheckProcess(t *testing.T) {
 	snc := StartTestAgent(t, "")
 
 	res := snc.RunCheck("check_process", []string{})
-	assert.Equalf(t, CheckExitOK, res.State, "state critical")
+	assert.Equalf(t, CheckExitOK, res.State, "state ok")
 	assert.Regexpf(t,
 		regexp.MustCompile(`^OK: all processes are ok`),
 		string(res.BuildPluginOutput()),
 		"output matches",
 	)
+
+	res = snc.RunCheck("check_process", []string{"process=noneexisting.exe"})
+	assert.Equalf(t, CheckExitUnknown, res.State, "state unknown")
+	assert.Equalf(t, "check_process failed to find anything with this filter. |'count'=0;;;0", string(res.BuildPluginOutput()), "output matches")
 
 	StopTestAgent(t, snc)
 }
