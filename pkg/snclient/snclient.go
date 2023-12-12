8d6f58f90e682e69ea0a50de8ecda5e0f254d00e
Sven Nierlein <sven@consol.de>
2023-12-12 19:57:46 +0100

    release v0.14
    Signed-off-by: Sven Nierlein <sven@consol.de>

--- pkg/snclient/snclient.go
+++ pkg/snclient/snclient.go
@@ -42,7 +42,7 @@ const (
 		" monitoring agent designed as replacement for NRPE and NSClient++."
 
 	// VERSION contains the actual snclient version.
-	VERSION = "0.13"
+	VERSION = "0.14"
 
 	// ExitCodeOK is used for normal exits.
 	ExitCodeOK = 0
