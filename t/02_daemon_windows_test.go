0d62c4d28dc9b90eb466f8eb4e398eb86b6ca09a
Sven Nierlein <sven@consol.de>
2024-02-13 21:33:04 +0100

    windows: add another e2e test

---
+++ t/02_daemon_windows_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDaemonRequestsLinux(t *testing.T) {
+	bin := getBinary()
+	require.FileExistsf(t, bin, "snclient binary must exist")
+
+	writeFile(t, `snclient.ini`, localDaemonINI)
+
+	startBackgroundDaemon(t)
+
+	baseURL := fmt.Sprintf("http://127.0.0.1:%d", localDaemonPort)
+
+	runCmd(t, &cmd{
+		Cmd:  "curl",
+		Args: []string{"-s", "-k", "--header", "password:" + localDaemonPassword, baseURL + "/query/check_echo_win?%20"},
+		Like: []string{`"check_echo_win"`, `"OK"`},
+		Exit: 0,
+	})
+
+	stopBackgroundDaemon(t)
+	os.Remove("snclient.ini")
+	os.Remove("test.crt")
+	os.Remove("test.key")
+}
