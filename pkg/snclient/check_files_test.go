8bf60edca6624367be312534102cbc5222dd7ec2
Sven Nierlein <sven@consol.de>
2024-02-19 15:42:15 +0100

    check_files: add more test cases

--- pkg/snclient/check_files_test.go
+++ pkg/snclient/check_files_test.go
@@ -1,9 +1,13 @@
 package snclient
 
 import (
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCheckFiles(t *testing.T) {
@@ -32,6 +36,37 @@ func TestCheckFiles(t *testing.T) {
 	res = snc.RunCheck("check_files", []string{"path=.", "filter=name eq 'check_files.go' and size gt 5K", "crit=count>0", "ok=count eq 0", "empty-state=ok"})
 	assert.Equalf(t, CheckExitCritical, res.State, "state Critical")
 	assert.Contains(t, string(res.BuildPluginOutput()), "'count'=")
+
+	StopTestAgent(t, snc)
+}
+
+func TestCheckFilesNoPermission(t *testing.T) {
+	snc := StartTestAgent(t, "")
+	// prepare test folder
+	tmpPath, err := os.MkdirTemp("", "testtmp*")
+	require.NoError(t, err)
+
+	for _, char := range []string{"a", "b", "c"} {
+		err = os.WriteFile(filepath.Join(tmpPath, "file "+char+".txt"), []byte(strings.Repeat(char, 2000)), 0o600)
+		require.NoErrorf(t, err, "writing %s worked")
+
+		err = os.Mkdir(filepath.Join(tmpPath, "dir "+char), 0o700)
+		require.NoErrorf(t, err, "writing %s worked")
+	}
+	err = os.Chmod(filepath.Join(tmpPath, "file b.txt"), 0o000)
+	require.NoError(t, err)
+	err = os.Chmod(filepath.Join(tmpPath, "dir b"), 0o000)
+	require.NoError(t, err)
+
+	res := snc.RunCheck("check_files", []string{"path=" + tmpPath, "filter=name eq 'file a.txt' and size gt 1K", "crit=count>0", "ok=count eq 0", "empty-state=ok"})
+	assert.Equalf(t, CheckExitCritical, res.State, "state Critical")
+	assert.Contains(t, string(res.BuildPluginOutput()), "'count'=")
+
+	res = snc.RunCheck("check_files", []string{"path=" + tmpPath})
+	assert.Equalf(t, CheckExitOK, res.State, "state OK")
+	assert.Contains(t, string(res.BuildPluginOutput()), "OK - All 3 files are ok: (5.86 KiB) |'count'=3;;;0 'size'=6000B;;;0")
+
+	defer os.RemoveAll(tmpPath)
 
 	StopTestAgent(t, snc)
 }
