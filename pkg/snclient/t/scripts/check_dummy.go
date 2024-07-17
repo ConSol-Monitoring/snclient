5f0b847640498b4214e2909d4b4ac5b2df914579
Sven Nierlein <sven@consol.de>
2024-07-17 11:05:22 +0200

    tests: unify check_dummy scripts
    only appen ": " if there is a message

--- pkg/snclient/t/scripts/check_dummy.go
+++ pkg/snclient/t/scripts/check_dummy.go
@@ -3,44 +3,37 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 )
 
 func main() {
 	if len(os.Args) < 2 {
-		//nolint:forbidigo // use of `fmt.Println` forbidden by pattern `^(fmt\.Print(|f|ln)|print|println)$`
-		fmt.Println("Invalid state argument. Please provide one of: 0, 1, 2, 3")
+		fmt.Fprintf(os.Stdout, "Invalid state argument. Please provide one of: 0, 1, 2, 3")
 		os.Exit(3)
 	}
 
-	state, err := strconv.Atoi(os.Args[1])
-	if err != nil || state < 0 || state > 3 {
-		//nolint:forbidigo // dito
-		fmt.Println("Invalid state argument. Please provide one of: 0, 1, 2, 3")
-		os.Exit(3)
-	}
-
-	var message string
+	message := ""
 	if len(os.Args) > 2 {
-		message = ": " + os.Args[2]
+		message = os.Args[2]
+	}
+	if message != "" {
+		message = ": " + message
 	}
 
 	exitStatus := 0
-	switch state {
-	case 0:
-		//nolint:forbidigo // dito
-		fmt.Println("OK" + message)
-	case 1:
-		//nolint:forbidigo // dito
-		fmt.Println("WARNING" + message)
+	switch os.Args[1] {
+	case "0":
+		fmt.Fprintf(os.Stdout, "OK"+message)
+	case "1":
+		fmt.Fprintf(os.Stdout, "WARNING"+message)
 		exitStatus = 1
-	case 2:
-		//nolint:forbidigo // dito
-		fmt.Println("CRITICAL" + message)
+	case "2":
+		fmt.Fprintf(os.Stdout, "CRITICAL"+message)
 		exitStatus = 2
-	case 3:
-		//nolint:forbidigo // dito
-		fmt.Println("UNKNOWN" + message)
+	case "3":
+		fmt.Fprintf(os.Stdout, "UNKNOWN"+message)
+		exitStatus = 3
+	default:
+		fmt.Fprintf(os.Stdout, "Invalid state argument. Please provide one of: 0, 1, 2, 3")
 		exitStatus = 3
 	}
 
