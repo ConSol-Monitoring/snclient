53a32ac1ffa93bec37d242b5310199d4f1902db5
Sven Nierlein <sven@consol.de>
2024-08-05 13:30:41 +0200

    fix crash in check_memory on windows in type virtual

--- pkg/snclient/check_memory_windows.go
+++ pkg/snclient/check_memory_windows.go
@@ -9,6 +9,9 @@ import (
 func (l *CheckMemory) virtualMemory() (total, avail uint64, err error) {
 	ex := mem.NewExWindows()
 	v, err := ex.VirtualMemory()
+	if err != nil {
+		return 0, 0, fmt.Errorf("fetching virtual memory failed: %s", err.Error())
+	}
 
-	return v.VirtualTotal, v.VirtualAvail, fmt.Errorf("fetching virtual memory failed: %s", err.Error())
+	return v.VirtualTotal, v.VirtualAvail, nil
 }
