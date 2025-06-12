a7eefb59e59ca63f5684501828e415d57435bfe7
Sven Nierlein <sven@consol.de>
2025-06-12 10:47:18 +0200

    fix race condition for node exporter start
    running state might be "Pending" and not yet "Started"

--- pkg/snclient/listen_managedexporter.go
+++ pkg/snclient/listen_managedexporter.go
@@ -204,7 +204,7 @@ func (l *HandlerManagedExporter) JSON() []map[string]string {
 }
 
 func (l *HandlerManagedExporter) keepRunning() bool {
-	return l.snc.IsRunning() && l.keepRunningA.Load()
+	return l.snc.running.Load() != Stopped && l.keepRunningA.Load()
 }
 
 func (l *HandlerManagedExporter) procMainLoop() {
