b41aafc6df33adb938d19773182e35e8d0042a24
Sven Nierlein <sven@consol.de>
2025-10-23 12:17:21 +0200

    tests: improve sensor pattern
    to match notebook sensors

--- pkg/snclient/check_temperature_linux_test.go
+++ pkg/snclient/check_temperature_linux_test.go
@@ -18,7 +18,7 @@ func TestTemperature(t *testing.T) {
 	}
 	assert.Equalf(t, CheckExitOK, res.State, "state ok")
 	assert.Regexpf(t,
-		`^OK -.*core_0: [\d.]+ °C`,
+		`^OK -.*(core_0|amdgpu_edge): [\d.]+ °C`,
 		string(res.BuildPluginOutput()),
 		"output matches",
 	)
