9b5c0c8338118b8ecdad18258f365bab961b8866
Ahmet Oeztuerk <Ahmet.Oeztuerk@consol.de>
2025-11-06 18:03:20 +0100

    - add a testing script for the check_files operands are given in natural language

--- pkg/snclient/check_files_test.go
+++ pkg/snclient/check_files_test.go
@@ -128,3 +128,78 @@ func TestCheckFilesNoPermission(t *testing.T) {
 
 	StopTestAgent(t, snc)
 }
+
+func check_files_configfile(t *testing.T, scriptsDir, scriptsType string) string {
+	t.Helper()
+
+	config := fmt.Sprintf(`
+[/modules]
+CheckExternalScripts = enabled
+
+[/paths]
+scripts = %s
+shared-path = %%(scripts)
+
+[/settings/external scripts/wrappings]
+sh = %%SCRIPT%% %%ARGS%%
+exe = %%SCRIPT%% %%ARGS%%
+
+[/settings/external scripts]
+timeout = 1111111
+allow arguments = true
+
+[/settings/external scripts/scripts]
+check_files_generate_files = ./check_files_generate_files.EXTENSION "$ARG1$"
+`, scriptsDir)
+
+	config = strings.ReplaceAll(config, "EXTENSION", scriptsType)
+
+	return config
+}
+
+func TestTimeKeywordFilters(t *testing.T) {
+
+	// prepare a tempdir
+	tempDir := t.TempDir()
+
+	fmt.Printf("tempDir= %s \n", tempDir)
+
+	testDir, _ := os.Getwd()
+	scriptsDir := filepath.Join(testDir, "t", "scripts")
+
+	config := check_files_configfile(t, scriptsDir, "sh")
+	snc := StartTestAgent(t, config)
+
+	// There is a bash script on this path: pkg/snclient/t/scripts/check_files_generate_files.sh
+	// It generates files on a temporary path, and changes their modification date
+	// This script is added to the snclient config first and registered as a check command, then ran by the snclient executable itself
+	res := snc.RunCheck("check_files_generate_files", []string{tempDir})
+
+	// one_year_from_now_on
+	// one_month_from_now_on
+	// one_week_from_now_on
+	// two_days_from_now_on
+	// tomorrow
+	// today
+	// yesterday
+	// two_days_ago
+	// one_week_ago
+	// one_month_ago
+	// one_year_ago
+	assert.Equalf(t, CheckExitOK, res.State, "Generating test files successfull")
+	assert.Equalf(t, string(res.BuildPluginOutput()), "ok - Generated 11 files for testing", "output matches")
+
+	// Note on 2025-11-06 : Multiple filter="<condition>"s are combined with a logical OR.
+	// res = snc.RunCheck("check_files", []string{fmt.Sprintf("path=%s", tempDir), "filter=\"written>=today\"", "filter=\"written<tomorrow\""})
+	// Such a test got every file
+
+	res = snc.RunCheck("check_files", []string{fmt.Sprintf("path=%s", tempDir), "filter=\"written=today\""})
+	assert.Equalf(t, CheckExitOK, res.State, "state OK")
+	assert.Containsf(t, string(res.BuildPluginOutput()), "OK - All 1 files are ok", "output matches")
+
+	res = snc.RunCheck("check_files", []string{fmt.Sprintf("path=%s", tempDir), "filter=\"written>today\""})
+	assert.Equalf(t, CheckExitOK, res.State, "state OK")
+	assert.Containsf(t, string(res.BuildPluginOutput()), "OK - All 5 files are ok", "output matches")
+
+	StopTestAgent(t, snc)
+}
---
+++ pkg/snclient/t/scripts/check_files_generate_files.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# This script is intended to generate files to be tested for check_files
+# These files need to be generated dynamically, so they cant be simply saved in the repository.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+TESTING_DIR="$1"
+
+mkdir -p ${TESTING_DIR}
+
+TODAY=$(date -d 'today 00:00:00' +%F)
+
+echo "${TODAY} ${TESTING_DIR}" > /tmp/asdasdasd
+
+(
+    cd ${TESTING_DIR}
+
+    touch --time modify --date $(date -d "${TODAY} - 1 years" +%FT%T) one_year_ago
+    touch --time modify --date $(date -d "${TODAY} - 1 months " +%FT%T) one_month_ago
+    touch --time modify --date $(date -d "${TODAY} - 1 week" +%FT%T) one_week_ago
+    touch --time modify --date $(date -d "${TODAY} - 2 days" +%FT%T) two_days_ago
+    touch --time modify --date $(date -d "${TODAY} - 1 days" +%FT%T) yesterday
+    touch --time modify --date $(date -d "${TODAY}" +%FT%T) today
+    touch --time modify --date $(date -d "${TODAY} + 1 days" +%FT%T) tomorrow
+    touch --time modify --date $(date -d "${TODAY} + 2 days" +%FT%T) two_days_from_now_on
+    touch --time modify --date $(date -d "${TODAY} + 1 weeks" +%FT%T) one_week_from_now_on
+    touch --time modify --date $(date -d "${TODAY} + 1 month" +%FT%T) one_month_from_now_on
+    touch --time modify --date $(date -d "${TODAY} + 1 years" +%FT%T) one_year_from_now_on
+
+    FILE_COUNT=$(find ${TESTING_DIR} -type f -printf "." | wc -c)
+
+    echo "ok - Generated ${FILE_COUNT} files for testing"
+)
+
+exit 0
+
