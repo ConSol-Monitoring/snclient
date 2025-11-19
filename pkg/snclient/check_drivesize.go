0a85a8b9612047fa94719de6b8c7e207129d6bbe
inqrphl <Ahmet.Oeztuerk@consol.de>
2025-11-19 16:53:42 +0100

    - added new errors for the windows helper files - also report ok for windows partition not found errors - add more tests for windows

--- pkg/snclient/check_drivesize.go
+++ pkg/snclient/check_drivesize.go
@@ -167,11 +167,15 @@ func (l *CheckDrivesize) Check(ctx context.Context, snc *Agent, check *CheckData
 	drives, err := l.getRequiredDisks(l.drives, false)
 	var notFoundErr *PartitionNotFoundError
 	var notMountedErr *PartitionNotMountedError
+	var partitionDiscoveryErr *PartitionDiscoveryError
 	switch {
 	case errors.As(err, &notFoundErr):
-		log.Debugf("check_drivesize, drive is not found : %s, continuing with empty disks", notFoundErr.Error())
+		log.Debugf("check_drivesize, drive is not found : %s, continuing check with empty disks", notFoundErr.Error())
 	case errors.As(err, &notMountedErr):
 		// no special handling of mount errors
+		return nil, err
+	case errors.As(err, &partitionDiscoveryErr):
+		// no special handling of discovery errors
 		return nil, err
 	case err != nil:
 		return nil, err
@@ -434,16 +438,26 @@ func (l *CheckDrivesize) getFlagNames(drive map[string]string) []string {
 // have to define the error variables here, this file builds on all platforms
 type PartitionNotFoundError struct {
 	Path string
+	err  error
 }
 
 func (e *PartitionNotFoundError) Error() string {
-	return fmt.Sprintf("partition not found for path: %s", e.Path)
+	return fmt.Sprintf("partition not found for path: %s , error: %s", e.Path, e.err.Error())
 }
 
 type PartitionNotMountedError struct {
 	Path string
+	err  error
 }
 
 func (e *PartitionNotMountedError) Error() string {
-	return fmt.Sprintf("partition not mounted for path: %s", e.Path)
+	return fmt.Sprintf("partition not mounted for path: %s , error: %s", e.Path, e.err.Error())
+}
+
+type PartitionDiscoveryError struct {
+	err error
+}
+
+func (e *PartitionDiscoveryError) Error() string {
+	return fmt.Sprintf("error on disk.partitions call: %s", e.err.Error())
 }
