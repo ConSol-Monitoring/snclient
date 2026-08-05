882176bc26ea50efafa5d87102826e3f600d58d1
Sven Nierlein <sven@consol.de>
2026-08-05 15:18:34 +0200

    windows: fix syntax error when using inline ps1 update check (fixes #443)

--- pkg/snclient/embed/scripts/windows/check_os_updates.ps1
+++ pkg/snclient/embed/scripts/windows/check_os_updates.ps1
@@ -1,13 +1,11 @@
 # check for windows updates
-# usage: .\check_os_updates.ps1 [ -Online ]
+# usage: .\check_os_updates.ps1
 #
 # docs:
 # https://learn.microsoft.com/en-us/windows/win32/api/wuapi/nf-wuapi-iupdatesearcher-search
 # https://learn.microsoft.com/en-us/windows/win32/api/wuapi/nn-wuapi-iupdate
 #
-param (
-    [switch]$Online
-)
+$Online = $false
 if ($env:ONLINE_SEARCH -eq "1") {
     $Online = $true
 }
